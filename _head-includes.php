61f93ead66bc1690d0d8ba667bc7e6cd333366ec
Mariya <alisa-m2@yandex.ru>
2019-07-22 01:28:45 +0300

    New css file

--- _head-includes.php
+++ _head-includes.php
@@ -10,4 +10,4 @@
   <!-- Custom Browsers Color End -->
 
   <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/main2.css">
